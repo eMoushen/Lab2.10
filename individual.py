5dbd87f80057a85f3023fa1c1fba48d77f059e02
eMoushen <strigalov1991@mail.ru>
2022-12-05 19:23:28 +0300

    update progr

--- individual.py
+++ individual.py
@@ -1,30 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-def multip(*args):
+def sump(*args):
     amin = 0
     amax = 0
-    maxim = 0
-    proiz = 1
     if args:
         for i in args:
-            if i > maxim:
-                maxim = i
+            if i > args[amax]:
                 amax = args.index(i)
-
-        mini = args[0]
-        for i in args:
-            if i < mini:
-                mini = i
+            elif i < args[amin]:
                 amin = args.index(i)
 
-        for i in args[amin+1:amax:]:
-            proiz *= i
-        return 'Произведение =', proiz
+        return sum([i for i in args[amin+1:amax:]])
 
     else:
         return None
 
 
 if __name__ == "__main__":
-    print(multip(10, 150, 5, 400, 13, 555))
+    print(sump())
+    print(sump(3, 5, 7, 12, 13, 10))
